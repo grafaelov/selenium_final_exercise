45a7a5f4a801e1b5a828482b358e6fe9df541c93
Georgy Rafaelov <Georgy.Rafaelov@kaspersky.com>
2020-04-21 16:31:46 +0300

    base_page.py is refactored

--- pages/locators.py
+++ pages/locators.py
@@ -1,9 +1,21 @@
 from selenium.webdriver.common.by import By
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    REGISTRATION_LINK = (By.CSS_SELECTOR, "#registration_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")   
+#class MainPageLocators():
+#    LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")   
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")   
+    NAME_BEFORE = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > h1")
+    PRICE_BEFORE = (By.CLASS_NAME, "price_color")
+    NAME_AFTER = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+    PRICE_AFTER = (By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div")
---
+++ pages/product_page.py
@@ -0,0 +1,42 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+from .locators import ProductPageLocators
+import time
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        self.name_before = self.browser.find_element(*ProductPageLocators.NAME_BEFORE).text  
+        self.price_before = self.browser.find_element(*ProductPageLocators.PRICE_BEFORE).text      
+        add_button = self.browser.find_element(*ProductPageLocators.ADD_BUTTON)
+        add_button.click()
+        self.solve_quiz_and_get_code()
+        self.name_after = self.browser.find_element(*ProductPageLocators.NAME_AFTER).text        
+        self.price_after = self.browser.find_element(*ProductPageLocators.PRICE_AFTER).text
+        self.should_be_correct_name()
+        self.should_be_correct_price()
+        
+    def add_to_basket_no_promo(self):
+        self.name_before = self.browser.find_element(*ProductPageLocators.NAME_BEFORE).text  
+        self.price_before = self.browser.find_element(*ProductPageLocators.PRICE_BEFORE).text      
+        add_button = self.browser.find_element(*ProductPageLocators.ADD_BUTTON)
+        add_button.click()
+        self.name_after = self.browser.find_element(*ProductPageLocators.NAME_AFTER).text        
+        self.price_after = self.browser.find_element(*ProductPageLocators.PRICE_AFTER).text
+        self.should_be_correct_name()
+        self.should_be_correct_price()
+        
+
+    def should_be_correct_name(self):
+        # реализуйте проверку на совпадение имени
+        assert self.name_before ==  self.name_after, f"Name mismatch, before operation the itne's name was '{self.name_before}', after add operation is '{self.name_after}'"
+
+    def should_be_correct_price(self):
+        # реализуйте проверку совпадения цены
+        assert self.price_before ==  self.price_after, f"Price mismatch, item price was '{self.price_before}', but after add operation basket the price is '{self.price_after}'"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
+
+    def should_be_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
+
